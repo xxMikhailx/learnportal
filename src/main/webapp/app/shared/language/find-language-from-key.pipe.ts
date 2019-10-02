89429f8b96e500100c6160a4a79b4c3a9c519da5
xxMikhailx <misha.kazuchits@gmail.com>
2019-10-03 01:20:34 +0300

    Remove i18n support

--- src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
+++ src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
@@ -3,7 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'findLanguageFromKey' })
 export class FindLanguageFromKeyPipe implements PipeTransform {
   private languages: any = {
-    en: { name: 'English' },
     ru: { name: 'Русский' }
     // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
   };
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -78,8 +78,7 @@ module.exports = (options) => ({
         new MergeJsonWebpackPlugin({
             output: {
                 groupBy: [
-                    { pattern: "./src/main/webapp/i18n/ru/*.json", fileName: "./i18n/ru.json" },
-                    { pattern: "./src/main/webapp/i18n/en/*.json", fileName: "./i18n/en.json" }
+                    { pattern: "./src/main/webapp/i18n/ru/*.json", fileName: "./i18n/ru.json" }
                     // jhipster-needle-i18n-language-webpack - JHipster will add/remove languages in this array
                 ]
             }
