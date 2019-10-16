4b968f1958c340636e2c5c64cf5dc8dddbe1ca30
xxMikhailx <misha.kazuchits@gmail.com>
2019-10-16 18:23:52 +0300

    Temporary fix editor css loading by adding it in CSP exceptions

--- src/main/java/com/litelife/learnportal/config/SecurityConfiguration.java
+++ src/main/java/com/litelife/learnportal/config/SecurityConfiguration.java
@@ -65,7 +65,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .accessDeniedHandler(problemSupport)
         .and()
             .headers()
-            .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:")
+            // TODO: Delete temporary links from CSP header
+            .contentSecurityPolicy("default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' https://cdn.syncfusion.com https://fonts.googleapis.com 'unsafe-inline'; img-src 'self' data:; font-src 'self' https://fonts.gstatic.com data:")
         .and()
             .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN)
         .and()
