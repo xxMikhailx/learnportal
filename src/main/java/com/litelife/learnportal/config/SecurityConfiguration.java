1ba9253d07076c3a76a5b780c7a41ce005b20e1f
xxMikhailx <misha.kazuchits@gmail.com>
2019-10-16 14:46:02 +0300

    Permit all to get task find data

--- src/main/java/com/litelife/learnportal/config/SecurityConfiguration.java
+++ src/main/java/com/litelife/learnportal/config/SecurityConfiguration.java
@@ -91,6 +91,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers(HttpMethod.GET,"/api/question-answers/**").permitAll()
             .antMatchers(HttpMethod.GET,"/api/quiz-questions/**").permitAll()
             .antMatchers(HttpMethod.GET,"/api/quizzes/**").permitAll()
+            .antMatchers(HttpMethod.GET,"/api/task-find-data/**").permitAll()
             .antMatchers("/api/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
